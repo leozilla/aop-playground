cc0e5c808927735106f89b4654db690b64ab6815
LEONHARTSBERGER David <david.leonhartsberger@frequentis.com>
2016-05-05 21:26:59 +0200

    Initial commit

---
+++ src/main/java/com/leonhart/aopplayground/Bootstrap.java
@@ -0,0 +1,16 @@
+package com.leonhart.aopplayground;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("Beans.xml");
+
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+
+        obj.getMessage();
+    }
+}
